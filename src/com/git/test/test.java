006294d3360a009125d93d732ec8f74d228965b9
xh <sitsea@163.com>
2023-01-12 19:11:06 +0800

    push commit

--- src/com/git/test/test.java
+++ src/com/git/test/test.java
@@ -1,18 +1,16 @@
 package com.git.test;
-
-import java.util.Map;
-
 /**
  * CLassName: test
  * Package: com.git.test
  * Description:
- *
- * @Author: GuRain
- * @Create: 2023/1/12 - 18:50
- * @Version: v1.0
+ * Author: GuRain
+ * Create: 2023/1/12 - 18:50
+ * Version: v1.0
  */
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        User u1 = new User("admin","管理员","123123");
+        System.out.println(u1);
     }
 }
