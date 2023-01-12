a154a77753b88a003aa3752253db49ac153883ed
xh <sitsea@163.com>
2023-01-12 18:52:32 +0800

    Initial commit

---
+++ src/com/git/test/test.java
@@ -0,0 +1,18 @@
+package com.git.test;
+
+import java.util.Map;
+
+/**
+ * CLassName: test
+ * Package: com.git.test
+ * Description:
+ *
+ * @Author: GuRain
+ * @Create: 2023/1/12 - 18:50
+ * @Version: v1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
